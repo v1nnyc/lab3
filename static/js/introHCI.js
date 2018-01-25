9ec66c1c6f5dc784c1a42ac9eccdfd9c6606a3a5
v1nnyc <cannallav019@yahoo.com>
2018-01-25 09:45:36 -0800

    i sure hope this isnt... this is real

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,25 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Arglé Barglé");
+        $('#testjs').text("Please wait...");
+        $(".jumbotron p").toggleClass("active");
+        $('#testjs').addClass("active");
+        $("a.thumbnail").click(projectClick);
 	});
 
-	// Add any additional listeners here
+        	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+}
+
+function projectClick(e){
+    e.preventDefault();
+
+    var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+    }
 }
